8f73fd343c01185236680c28bd320283e7cdd932
carefreedavid <david@made.co.za>
2017-05-24 16:30:08 +0200

    small changes

--- working/csv.js
+++ working/csv.js
@@ -1,6 +1,6 @@
 var Papa = require('papaparse'); // npm install papaparse
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; // npm install xmlhttprequest (install instructions: https://www.npmjs.com/package/xmlhttprequest)
-var xhr = new XMLHttpRequest("https://firebasestorage.googleapis.com/v0/b/jian-yang-57bf8.appspot.com/o/normal.csv?alt=media&token=e3f6a983-51ef-454d-95cd-517006326c70");
+var xhr = new XMLHttpRequest();
 
 
 // I don't know which is the best link to use from firebase
